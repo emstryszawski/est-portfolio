31a0273356da92e595fa0bd4626eb8a385250554
Emil Stryszawski <stryszawskiemil@gmail.com>
2024-03-10 19:10:30 +0100

    experience section

--- js/app.js
+++ js/app.js
@@ -1,36 +1,24 @@
 function init() {
-  showJobDescription();
+    showJob("bec");
 }
 
-function showJobDescription() {
-  const jobTitle = document.getElementById("jobTitle");
-  const jobTimeSpan = document.getElementById("jobTimeSpan");
-  const jobDescription = document.getElementById("jobDescription");
+function showJob(companyName) {
+    const jobTitle = document.getElementById("jobTitle");
+    const jobTimeSpan = document.getElementById("jobTimeSpan");
+    const jobDescription = document.getElementById("jobDescription");
 
-  const becCheckBox = document.getElementById("bec");
-  const neoSmartCheckBox = document.getElementById("neoSmart");
-
-  if (becCheckBox.checked) {
-    readJobDescription("resources/bec.json")
-      .then((job) => {
-        jobDescription.textContent = job["description"];
-        jobTitle.textContent = job["title"];
-        jobTimeSpan.textContent = job["timeSpan"];
-      });
-  } else if (neoSmartCheckBox.checked) {
-    readJobDescription("resources/neoSmart.json")
-      .then((job) => {
-        jobDescription.textContent = job["description"];
-        jobTitle.textContent = job["title"];
-        jobTimeSpan.textContent = job["timeSpan"];
-      });
-  }
+    readJobDescription("resources/" + companyName + ".json")
+        .then((job) => {
+            jobDescription.textContent = job["description"];
+            jobTitle.textContent = job["title"];
+            jobTimeSpan.textContent = job["timeSpan"];
+        })
 }
 
 function readJobDescription(textPath) {
-  return fetch(textPath)
-    .then((response) => response.json())
-    .catch((error) => console.error(error));
+    return fetch(textPath)
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
 }
 
 
